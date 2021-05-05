bc955f6ef4e6cf761930ab5cf2b1763ffdc59d93
WangMaoXiong <39901449+wangmaoxiong@users.noreply.github.com>
2021-05-05 11:57:31 +0800

    Update RedDoorApplication.java
    添加注释

--- src/main/java/com/wmx/reddoor/RedDoorApplication.java
+++ src/main/java/com/wmx/reddoor/RedDoorApplication.java
@@ -3,6 +3,7 @@ package com.wmx.reddoor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 启动类
 @SpringBootApplication
 public class RedDoorApplication {
 
